ae8b54ec007d6e04f5d00abbba49d02714b556d8
nwafor6 <nwaforglory6@gmail.com>
2024-06-12 14:10:07 +0100

    switched baseurl to prod

--- baseUrl.js
+++ baseUrl.js
@@ -1,3 +1,3 @@
-// const baseUrl=`https://formaxion-ba87a7dd8dbd.herokuapp.com`
-const baseUrl=`http://127.0.0.1:8000`
+const baseUrl=`https://formaxion-ba87a7dd8dbd.herokuapp.com`
+// const baseUrl=`http://127.0.0.1:8000`
 // const socketUrl=`127.0.0.1:8000`
